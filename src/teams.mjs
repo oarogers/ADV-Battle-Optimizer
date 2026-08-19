eb6eceb644b5389cc7d2948b47738a4a511ee2cc
oarogers <47584074+oarogers@users.noreply.github.com>
2026-08-19 13:48:30 -0700

    Add ADV optimizer project files

---
+++ src/teams.mjs
@@ -0,0 +1,101 @@
+export const team1 = [
+  {
+    name: "Tyranitar", species: "Tyranitar", item: "Leftovers",
+    ability: "Sand Stream",
+    moves: ["Rock Slide", "Earthquake", "Hidden Power Grass", "Roar"],
+    nature: "Adamant",
+    evs: {hp:252, atk:176, def:0, spa:0, spd:80, spe:0},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Skarmory", species: "Skarmory", item: "Leftovers",
+    ability: "Keen Eye",
+    moves: ["Spikes", "Roar", "Protect", "Drill Peck"],
+    nature: "Impish",
+    evs: {hp:252, atk:0, def:252, spa:0, spd:4, spe:0},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Gengar", species: "Gengar", item: "Leftovers",
+    ability: "Levitate",
+    moves: ["Thunderbolt", "Ice Punch", "Giga Drain", "Will-O-Wisp"],
+    nature: "Timid",
+    evs: {hp:0, atk:0, def:0, spa:252, spd:4, spe:252},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Swampert", species: "Swampert", item: "Leftovers",
+    ability: "Torrent",
+    moves: ["Earthquake", "Ice Beam", "Hydro Pump", "Protect"],
+    nature: "Relaxed",
+    evs: {hp:240, atk:52, def:216, spa:0, spd:0, spe:0},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Blissey", species: "Blissey", item: "Leftovers",
+    ability: "Natural Cure",
+    moves: ["Seismic Toss", "Ice Beam", "Soft-Boiled", "Thunder Wave"],
+    nature: "Bold",
+    evs: {hp:252, atk:0, def:252, spa:0, spd:4, spe:0},
+    ivs: {hp:31, atk:0, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Metagross", species: "Metagross", item: "Leftovers",
+    ability: "Clear Body",
+    moves: ["Meteor Mash", "Earthquake", "Explosion", "Agility"],
+    nature: "Adamant",
+    evs: {hp:252, atk:232, def:0, spa:0, spd:0, spe:24},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  }
+];
+
+export const team2 = [
+  {
+    name: "Tyranitar", species: "Tyranitar", item: "Leftovers",
+    ability: "Sand Stream",
+    moves: ["Dragon Dance", "Rock Slide", "Earthquake", "Hidden Power Bug"],
+    nature: "Adamant",
+    evs: {hp:0, atk:252, def:0, spa:0, spd:4, spe:252},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Zapdos", species: "Zapdos", item: "Leftovers",
+    ability: "Pressure",
+    moves: ["Thunderbolt", "Hidden Power Grass", "Roar", "Rest"],
+    nature: "Timid",
+    evs: {hp:0, atk:0, def:0, spa:252, spd:4, spe:252},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Dugtrio", species: "Dugtrio", item: "Choice Band",
+    ability: "Arena Trap",
+    moves: ["Earthquake", "Rock Slide", "Aerial Ace", "Hidden Power Bug"],
+    nature: "Jolly",
+    evs: {hp:0, atk:252, def:4, spa:0, spd:0, spe:252},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Celebi", species: "Celebi", item: "Leftovers",
+    ability: "Natural Cure",
+    moves: ["Psychic", "Giga Drain", "Leech Seed", "Recover"],
+    nature: "Bold",
+    evs: {hp:252, atk:0, def:220, spa:0, spd:36, spe:0},
+    ivs: {hp:31, atk:0, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Swampert", species: "Swampert", item: "Leftovers",
+    ability: "Torrent",
+    moves: ["Earthquake", "Ice Beam", "Hydro Pump", "Protect"],
+    nature: "Relaxed",
+    evs: {hp:240, atk:52, def:216, spa:0, spd:0, spe:0},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  },
+  {
+    name: "Gengar", species: "Gengar", item: "Leftovers",
+    ability: "Levitate",
+    moves: ["Thunderbolt", "Ice Punch", "Giga Drain", "Will-O-Wisp"],
+    nature: "Timid",
+    evs: {hp:0, atk:0, def:0, spa:252, spd:4, spe:252},
+    ivs: {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}, level:100
+  }
+];
